8d8763f3a2f78146876b7f343358b73edcacc34c
Marcin <marcin.jasinski1997@gmail.com>
2018-11-08 18:47:53 +0100

    Brute-force refactor.

--- Exact Algorithms/Exact Algorithms/BruteForce.cpp
+++ Exact Algorithms/Exact Algorithms/BruteForce.cpp
@@ -12,6 +12,52 @@
 int bestTotalCost;
 Array bestRoute;
 
+LARGE_INTEGER li;
+double PCFreq = double(li.QuadPart) / 1000000.0;
+__int64 CounterStart = 0;
+
+void bruteForce(int citiesNumber, int **edgesMatrix)
+{
+	Array Numbers_array = Array();
+	for (int i = 1; i <= citiesNumber; i++) Numbers_array.pushBack(i);
+
+	double endTime;
+	bestTotalCost = INT32_MAX;
+	bestRoute = NULL;
+
+	std::cout << "\nProcessing, please wait... (if data set is large enough, you have really a lot of time now)" << std::endl;
+	StartCounter();
+	permutate(Numbers_array, edgesMatrix, 0);
+	endTime = GetCounter();
+	std::cout << "\n Elapsed time: " << endTime << " seconds." << std::endl;
+	std::cout << " Best route cost: " << bestTotalCost << std::endl;
+	std::cout << " Path: " << bestRoute << std::endl;
+	return;
+}
+
+void permutate(Array numbers, int** edgesMatrix, unsigned int index)
+{
+	if (index == numbers.getSize())
+	{
+		calculateTotalRouteCost(numbers, edgesMatrix);
+		return;
+	}
+
+	for (int i = index; i < numbers.getSize(); i++)
+	{
+		swapElements(numbers, i, index);
+		permutate(numbers, edgesMatrix, index + 1);
+		swapElements(numbers, i, index);
+	}
+}
+
+void swapElements(Array array, int indexA, int indexB)
+{
+	int x = array.get(indexA);
+	array.replaceValueOnIndex(indexA, array.get(indexB));
+	array.replaceValueOnIndex(indexB, x);
+}
+
 void calculateTotalRouteCost(Array numbers, int** edgesMatrix)
 {
 	int totalCost = 0;
@@ -40,33 +86,6 @@ void calculateTotalRouteCost(Array numbers, int** edgesMatrix)
 	return;
 }
 
-void swapElements(Array array, int indexA, int indexB)
-{
-	int x = array.get(indexA);
-	array.replaceValueOnIndex(indexA, array.get(indexB));
-	array.replaceValueOnIndex(indexB, x);
-}
-
-void permutate(Array numbers, int** edgesMatrix, unsigned int index)
-{
-	if (index == numbers.getSize()) // display vector if you have reached last element 
-	{
-		calculateTotalRouteCost(numbers, edgesMatrix);
-		return;
-	}
-
-	for (int i = index; i < numbers.getSize(); i++)
-	{
-		swapElements(numbers, i, index);	
-		permutate(numbers, edgesMatrix, index + 1);
-		swapElements(numbers, i, index);	
-	}
-}
-
-LARGE_INTEGER li;
-double PCFreq = double(li.QuadPart) / 1000000.0;
-__int64 CounterStart = 0;
-
 void StartCounter()
 {
 	LARGE_INTEGER li;
@@ -85,24 +104,7 @@ double GetCounter()
 	return double(li.QuadPart - CounterStart) / PCFreq;
 }
 
-void bruteForce(int citiesNumber, int **edgesMatrix)
-{
-	Array Numbers_array = Array();
-	for (int i = 1; i <= citiesNumber; i++) Numbers_array.pushBack(i);
 
-	double endTime;
-	bestTotalCost = INT32_MAX;
-	bestRoute = NULL;
-
-	std::cout << "\nProcessing, please wait... (if data set is large enough, you have really a lot of time now)" << std::endl;
-	StartCounter();
-	permutate(Numbers_array, edgesMatrix, 0);
-	endTime = GetCounter();
-	std::cout << "\n Elapsed time: " << endTime << " seconds." << std::endl;
-	std::cout << " Best route cost: " << bestTotalCost << std::endl;
-	std::cout << " Path: " << bestRoute << std::endl;
-	return;
-}
 
 
 
--- Exact Algorithms/Exact Algorithms/BruteForce.h
+++ Exact Algorithms/Exact Algorithms/BruteForce.h
@@ -1,4 +1,9 @@
 #pragma once
+#include "Array.h"
 
 void bruteForce(int, int**);
-
+void calculateTotalRouteCost(Array, int**);
+void swapElements(Array, int, int);
+void permutate(Array, int**, unsigned int);
+void StartCounter();
+double GetCounter();
