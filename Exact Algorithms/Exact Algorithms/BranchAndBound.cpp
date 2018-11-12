5020690cab42a29413541c91a17a77c28b96535b
Marcin <marcin.jasinski1997@gmail.com>
2018-11-13 00:40:53 +0100

    Further improvements.

--- Exact Algorithms/Exact Algorithms/BranchAndBound.cpp
+++ Exact Algorithms/Exact Algorithms/BranchAndBound.cpp
@@ -1,4 +1,5 @@
 #include "pch.h"
+#include "BranchAndBound.h"
 #include "Stopwatch.h"
 #include "Node.h"
 
@@ -16,55 +17,114 @@
 #include <cstring>
 #include <climits>
 
-int* reduceMatrixRow(int** reducedMatrix, int citiesNumber)
+// This method starts the main B&B algorithm execution.
+int branchAndBound(int citiesNumber, int** costMatrix)
 {
-	int* row = new int[citiesNumber];
-	for (int i = 0; i < citiesNumber; i++)
+	std::priority_queue<Node*, std::vector<Node*>, comp> nodesPriorityQueue;
+	std::vector<int> pathToNode;
+
+	Node* startingNode = new Node(costMatrix, pathToNode, 0, -1, 0, citiesNumber);
+
+	startingNode->lowerBound = calculateLowerBound(startingNode->nodeMatrix, citiesNumber);
+
+	nodesPriorityQueue.push(startingNode);
+
+	while (!nodesPriorityQueue.empty())
 	{
-		row[i] = INT_MAX;
+		Node* min = nodesPriorityQueue.top();
+		nodesPriorityQueue.pop();
+
+		int i = min->currentCityNumber;
+
+		if (min->graphLevel == citiesNumber - 1)
+		{
+			min->pathToVertex.push_back(0);
+
+			std::cout << "=== Branch & Bound ===" << std::endl;
+			std::cout << " Best route cost: " << min->lowerBound << std::endl;
+			std::cout << " Optimal path: ";
+
+			for (int i = min->pathToVertex.size() - 1; i >= 0; i--)
+			{
+				std::cout << min->pathToVertex[i] << " -> ";
+			}
+			std::cout << min->pathToVertex[min->pathToVertex.size() - 1] << std::endl;
+
+			nodesPriorityQueue.empty();
+			return min->lowerBound;
+		}
+
+		for (int j = 0; j < citiesNumber; j++)
+		{
+			if (min->nodeMatrix[i][j] != INT_MAX)
+			{
+				Node* child = new Node(min->nodeMatrix, min->pathToVertex, min->graphLevel + 1, i, j, citiesNumber);
+				child->lowerBound = min->lowerBound + min->nodeMatrix[i][j] + calculateLowerBound(child->nodeMatrix, citiesNumber);
+				nodesPriorityQueue.push(child);
+			}
+		}
+
+		delete min;
 	}
 
-	// row[i] contains minimum in row i
-	for (int i = 0; i < citiesNumber; i++)
-		for (int j = 0; j < citiesNumber; j++)
-			if (reducedMatrix[i][j] < row[i])
-				row[i] = reducedMatrix[i][j];
+	return -1;
+}
 
-	// reduce the minimum value from each element in each row
+int* reduceMatrixRow(int** reducedMatrix, int citiesNumber)
+{
+	int* reductionRow = new int[citiesNumber];
 	for (int i = 0; i < citiesNumber; i++)
-		for (int j = 0; j < citiesNumber; j++)
-			if (reducedMatrix[i][j] != INT_MAX && row[i] != INT_MAX)
-				reducedMatrix[i][j] -= row[i];
+	{
+		reductionRow[i] = INT_MAX;
+	}
 
-	return row;
+	for (int i = 0; i < citiesNumber; i++)
+	{
+		for (int j = 0; j < citiesNumber; j++)
+		{
+			if (reducedMatrix[i][j] < reductionRow[i]) reductionRow[i] = reducedMatrix[i][j];
+		}
+	}
+
+	for (int i = 0; i < citiesNumber; i++)
+	{
+		for (int j = 0; j < citiesNumber; j++)
+		{
+			if (reducedMatrix[i][j] != INT_MAX && reductionRow[i] != INT_MAX) reducedMatrix[i][j] -= reductionRow[i];
+		}
+	}
+
+	return reductionRow;
 }
 
 int* reduceMatrixCol(int** reducedMatrix, int citiesNumber)
 {
-	int* col = new int[citiesNumber];
+	int* reductionCol = new int[citiesNumber];
 	for (int i = 0; i < citiesNumber; i++)
 	{
-		col[i] = INT_MAX;
+		reductionCol[i] = INT_MAX;
 	}
 
-	// col[j] contains minimum in col j
 	for (int i = 0; i < citiesNumber; i++)
+	{
 		for (int j = 0; j < citiesNumber; j++)
-			if (reducedMatrix[i][j] < col[j])
-				col[j] = reducedMatrix[i][j];
-
-	// reduce the minimum value from each element in each column
+		{
+			if (reducedMatrix[i][j] < reductionCol[j]) reductionCol[j] = reducedMatrix[i][j];
+		}
+	}
+		
 	for (int i = 0; i < citiesNumber; i++)
+	{
 		for (int j = 0; j < citiesNumber; j++)
-			if (reducedMatrix[i][j] != INT_MAX && col[j] != INT_MAX)
-				reducedMatrix[i][j] -= col[j];
+		{
+			if (reducedMatrix[i][j] != INT_MAX && reductionCol[j] != INT_MAX) reducedMatrix[i][j] -= reductionCol[j];
+		}
+	}
 
-	return col;
+	return reductionCol;
 }
 
-// Function to get the lower bound on
-// on the path starting at current min node
-int calculateCost(int** reducedMatrix, int citiesNumber)
+int calculateLowerBound(int** reducedMatrix, int citiesNumber)
 {
 	int cost = 0;
 	int* reductionRow = reduceMatrixRow(reducedMatrix,citiesNumber);
@@ -82,72 +142,4 @@ int calculateCost(int** reducedMatrix, int citiesNumber)
 	return cost;
 }
 
-struct comp {
-	bool operator()(const Node* nodeA, const Node* nodeB) const
-	{
-		return nodeA->lowerBound > nodeB->lowerBound;
-	}
-};
 
-// Function to solve Traveling Salesman Problem using Branch and Bound
-int branchAndBound(int citiesNumber, int** costMatrix)
-{
-	std::priority_queue<Node*, std::vector<Node*>, comp> pq;
-	std::vector<int> v;
-
-	Node* root = new Node(costMatrix, v, 0, -1, 0, citiesNumber);
-
-	root->lowerBound = calculateCost(root->nodeMatrix, citiesNumber);
-
-	// Add root to list of live nodes;
-	pq.push(root);
-
-	// Finds a live node with least cost, add its children to list of
-	// live nodes and finally deletes it from the list
-	while (!pq.empty())
-	{
-		// Find a live node with least estimated cost
-		Node* min = pq.top();
-
-		// The found node is deleted from the list of live nodes
-		pq.pop();
-
-		// i stores current city number
-		int i = min->currentCityNumber;
-
-		// if all cities are visited
-		if (min->graphLevel == citiesNumber - 1)
-		{
-			// return to starting city
-			min->pathToVertex.push_back(0);
-
-			// return optimal cost
-			std::cout << "=== Branch & Bound ===" << std::endl;
-			std::cout << " Best route cost: " << min->lowerBound << std::endl;
-			std::cout << " Optimal path: ";
-
-			for (int i = min->pathToVertex.size() - 1; i >= 0; i--)
-			{
-				std::cout << min->pathToVertex[i] << " -> ";
-			}
-			std::cout << min->pathToVertex[min->pathToVertex.size() - 1] << std::endl;
-			
-			pq.empty();
-			return min->lowerBound;
-		}
-
-		for (int j = 0; j < citiesNumber; j++)
-		{
-			if (min->nodeMatrix[i][j] != INT_MAX)
-			{
-				Node* child = new Node(min->nodeMatrix, min->pathToVertex, min->graphLevel+ 1, i, j, citiesNumber);
-				child->lowerBound = min->lowerBound + min->nodeMatrix[i][j] + calculateCost(child->nodeMatrix, citiesNumber);
-				pq.push(child);
-			}
-		}
-
-		delete min;
-	}
-
-	return -1;
-}
--- Exact Algorithms/Exact Algorithms/BranchAndBound.h
+++ Exact Algorithms/Exact Algorithms/BranchAndBound.h
@@ -1,3 +1,15 @@
 #pragma once
+#include "Node.h"
+
+// This structure is used as a comparator for nodes priority queue.
+struct comp {
+	bool operator()(const Node* nodeA, const Node* nodeB) const
+	{
+		return nodeA->lowerBound > nodeB->lowerBound;
+	}
+};
 
 int branchAndBound(int citiesNumber, int** costMatrix);
+int* reduceMatrixRow(int** reducedMatrix, int citiesNumber);
+int* reduceMatrixCol(int** reducedMatrix, int citiesNumber);
+int calculateLowerBound(int** reducedMatrix, int citiesNumber);
