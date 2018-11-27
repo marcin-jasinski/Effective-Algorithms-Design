4f905247c337d610dfe88c71d66aeb60f4ae4f92
Marcin <marcin.jasinski1997@gmail.com>
2018-11-28 00:33:39 +0100

    Working Simulated Annealing, tested for instances up to 170 locations.

--- Metaheuristic/Metaheuristic/SimulatedAnnealing.h
+++ Metaheuristic/Metaheuristic/SimulatedAnnealing.h
@@ -1,2 +1,8 @@
 #pragma once
+#include <vector>
+
+std::vector<int> getRandomPermutation(int);
+int getRouteCost(std::vector<int>, int**, int);
+
+std::vector<int> simulatedAnnealing(double maxTemperature, double tempChange, int citiesNumber, int** edgesMatrix);
 
