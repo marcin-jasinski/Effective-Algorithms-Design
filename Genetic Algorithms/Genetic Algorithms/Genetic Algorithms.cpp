4195e263ae6717df803ad5e6e1c2cc8d75e66a84
Marcin <marcin.jasinski1997@gmail.com>
2019-01-09 23:45:02 +0100

    Added PMX crossover function.

--- Genetic Algorithms/Genetic Algorithms/Genetic Algorithms.cpp
+++ Genetic Algorithms/Genetic Algorithms/Genetic Algorithms.cpp
@@ -8,6 +8,14 @@
 #include <string>
 #include <random>
 
+void printSpecimen(std::vector<int>);
+void printPath(std::vector<int>);
+int** readAsymetricTSPData(std::string);
+int** readSymetricTSPData(std::string);
+int nint(double d);
+double convert_to_geo(double);
+int TWOD_geo_distance(const Point, const Point);
+
 int** edgesMatrix;
 int citiesNumber;
 int correctSolution;
@@ -19,12 +27,28 @@ std::vector<int> solution;
 
 std::vector<int> getNextSpecimen(int);
 std::vector<std::vector<int>> getStartingPopulation(int, int);
+std::vector<std::vector<int>> crossoverPopulation(std::vector<std::vector<int>>, int, int);
 int getRouteCost(std::vector<int>);
 
 int main()
 {
 	srand(time(NULL));
-    std::cout << "Hello World!\n"; 
+
+	std::cout << "Starting genetic algorithm... " << std::endl;
+
+	edgesMatrix = readSymetricTSPData("TSP_Data/burma14.tsp");
+	correctSolution = 3323;
+
+	std::vector<std::vector<int>> population;
+
+	population = getStartingPopulation(2, citiesNumber);
+	printSpecimen(population[0]);
+	printSpecimen(population[1]);
+
+	std::cout << "Crosssss...." << std::endl;
+	population = crossoverPopulation(population, 2, citiesNumber);
+	printSpecimen(population[0]);
+	printSpecimen(population[1]);
 
 	std::getchar();
 	return 0;
@@ -44,7 +68,7 @@ std::vector<int> getNextSpecimen(int problemSize)
 
 		specimen.push_back(genome);
 	}
-	
+
 	return specimen;
 }
 
@@ -59,6 +83,104 @@ std::vector<std::vector<int>> getStartingPopulation(int populationSize, int prob
 	}
 
 	return population;
+}
+
+std::vector<std::vector<int>> crossoverPopulation(std::vector<std::vector<int>> population, int populationSize, int problemSize)
+{
+	int targetPopulationSize = population.size();
+	int currentPopulationSize = 0;
+
+	std::vector<std::vector<int>> newPopulation;
+
+	// PMX
+	std::vector<int> alreadyCrossed;
+
+	while (currentPopulationSize < targetPopulationSize)
+	{
+		int crossStartPoint = rand() % problemSize + 1;
+		int crossEndPoint;
+		do { crossEndPoint = rand() % problemSize + 1; } while (crossEndPoint <= crossStartPoint);
+
+		int parent1_index, parent2_index;
+		while (true)
+		{
+			parent1_index = (rand() % populationSize);
+			parent2_index = (rand() % populationSize);
+
+			if (parent1_index != parent2_index && //
+				std::find(alreadyCrossed.begin(), alreadyCrossed.end(), parent1_index) == alreadyCrossed.end() && //
+				std::find(alreadyCrossed.begin(), alreadyCrossed.end(), parent2_index) == alreadyCrossed.end())
+			{
+				break;
+			}
+		}
+
+		alreadyCrossed.push_back(parent1_index);
+		alreadyCrossed.push_back(parent2_index);
+
+		std::vector<int> parent1 = population.at(parent1_index);
+		std::vector<int> parent2 = population.at(parent2_index);
+		std::vector<std::pair<int, int>> modelTable;
+
+		for (int i = crossStartPoint; i < crossEndPoint; i++)
+		{
+			auto modelPair = std::make_pair(parent1.at(i), parent2.at(i));
+			modelTable.push_back(modelPair);
+		}
+
+		std::vector<int> child1(parent1.begin(), parent1.end());
+		std::vector<int> child2(parent2.begin(), parent2.end());
+
+		std::fill(child1.begin(), child1.end(), -1);
+		std::fill(child2.begin(), child2.end(), -1);
+
+		// initial crossing
+		for (int i = crossStartPoint; i < crossEndPoint; i++)
+		{
+			child1[i] = parent2[i];
+			child2[i] = parent1[i];
+		}
+
+		// inserting values with no conflict
+		for (int i = 1; i <= problemSize; i++)
+		{
+			if (std::find(child1.begin(), child1.end(), i) == child1.end())
+			{
+				auto it = std::find(parent1.begin(), parent1.end(), i);
+				auto index = std::distance(parent1.begin(), it);
+				child1[index] = i;
+			}
+
+			if (std::find(child2.begin(), child2.end(), i) == child2.end())
+			{
+				auto it = std::find(parent2.begin(), parent2.end(), i);
+				auto index = std::distance(parent2.begin(), it);
+				child2[index] = i;
+			}
+		}
+
+		// inserting values from model table
+		for (int i = 0; i < modelTable.size(); i++)
+		{
+			auto currentPair = modelTable.at(i);
+
+			auto it_first = std::find(parent2.begin(), parent2.end(), currentPair.first);
+			auto index_first = std::distance(parent2.begin(), it_first);
+
+			auto it_second = std::find(parent1.begin(), parent1.end(), currentPair.second);
+			auto index_second = std::distance(parent1.begin(), it_second);
+
+			child1[index_second] = currentPair.second;
+			child2[index_first] = currentPair.first;
+		}
+
+		newPopulation.push_back(child1);
+		newPopulation.push_back(child2);
+
+		currentPopulationSize += 2;
+	}
+
+	return newPopulation;
 }
 
 int getRouteCost(std::vector<int> specimen, int** edgesMatrix, int citiesNumber)
@@ -77,6 +199,17 @@ int getRouteCost(std::vector<int> specimen, int** edgesMatrix, int citiesNumber)
 	totalCost += edgesMatrix[lastCityIndex][startingCityIndex];
 
 	return totalCost;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void printSpecimen(std::vector<int> specimen)
+{
+	for (int i = 0; i < specimen.size(); i++)
+	{
+		std::cout << specimen[i] << " ";
+	}
+	std::cout << std::endl;
 }
 
 void printPath(std::vector<int> solution)
